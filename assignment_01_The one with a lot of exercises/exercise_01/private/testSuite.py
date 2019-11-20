f5bd22ff7df4a20165544bc032fb3a5edfea0427
Alexander Hofmann <alexhofmann@gmail.com>
2019-11-20 17:21:49 +0100

    small change

--- assignment_01_The one with a lot of exercises/exercise_01/private/testSuite.py
+++ assignment_01_The one with a lot of exercises/exercise_01/private/testSuite.py
@@ -1,59 +1,92 @@
 from unittest import TestCase
 
-from public.script import Aircraft, IntercontinentalAircraft, ShortHaulAircraft, ControlTower
+#from public.script import Aircraft, IntercontinentalAircraft, ShortHaulAircraft, ControlTower
 
+import os
+import importlib
+import importlib.util
+from unittest.mock import patch
+
+devnull = open(os.devnull, 'w')
+
+
+def reload_module(module, path, user_input=(), outstream=devnull):
+    with patch('sys.stdout', outstream):
+        with patch('sys.stderr', outstream):
+            with patch('builtins.input', side_effect=user_input):
+                return load_module_from_file(module, path)
+
+
+def load_module_from_file(module_name, file_path, input_side_effect=()):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+
+    if input_side_effect:
+        with patch('builtins.input', side_effect=input_side_effect):
+            spec.loader.exec_module(module)
+    else:
+        spec.loader.exec_module(module)
+    return module
 
 class Task1Test(TestCase):
 
-    def test_inheritance(self):
-        intercontinental_flight = IntercontinentalAircraft(40, "intercontinental", 100)
-        short_haul_flight = ShortHaulAircraft(90, "short")
+    CTX = ''
+    MODULE_NAME = 'public.script'
+    TEST_FILE_PATH = './public/script.py'
 
-        self.assertTrue(isinstance(intercontinental_flight, Aircraft),
+    def setUp(self):
+        print
+        self.student_submission = reload_module(self.MODULE_NAME, './public/script.py', [])
+
+    def test_inheritance(self):
+        intercontinental_flight = self.student_submission.IntercontinentalAircraft(40, "intercontinental", 100)
+        short_haul_flight = self.student_submission.ShortHaulAircraft(90, "short")
+
+        self.assertTrue(isinstance(intercontinental_flight, self.student_submission.Aircraft),
                         "@#@IntercontinentalAircraft should inherit from Aircraft@#@")
-        self.assertTrue(isinstance(short_haul_flight, Aircraft), "@#@ShortHaulAircraft should inherit from Aircraft@#@")
+        self.assertTrue(isinstance(short_haul_flight, self.student_submission.Aircraft), "@#@ShortHaulAircraft should inherit from Aircraft@#@")
 
     def test_aircraft_get_name(self):
-        aircraft = IntercontinentalAircraft(40, "intercontinental", 100)
+        aircraft = self.student_submission.IntercontinentalAircraft(40, "intercontinental", 100)
         self.assertEqual(aircraft.get_name(), "intercontinental")
 
-        aircraft = ShortHaulAircraft(90, "short")
+        aircraft = self.student_submission.ShortHaulAircraft(90, "short")
         self.assertEqual(aircraft.get_name(), "short")
 
     def test_aircraft_get_number_of_passengers(self):
-        aircraft = IntercontinentalAircraft(40, "intercontinental", 100)
+        aircraft = self.student_submission.IntercontinentalAircraft(40, "intercontinental", 100)
         self.assertEqual(aircraft.get_number_of_passengers(), 40)
 
-        aircraft = ShortHaulAircraft(90, "short")
+        aircraft = self.student_submission.ShortHaulAircraft(90, "short")
         self.assertEqual(aircraft.get_number_of_passengers(), 90)
 
     def test_calculate_amount_of_fuel_intercontinental(self):
-        aircraft = IntercontinentalAircraft(40, "intercontinental", 100)
+        aircraft = self.student_submission.IntercontinentalAircraft(40, "intercontinental", 100)
         fuel = aircraft.calculate_amount_of_fuel(1000)
         self.assertEqual(fuel, 210000)
 
     def test_get_manifest_intercontinental(self):
-        aircraft = IntercontinentalAircraft(40, "intercontinental", 100)
+        aircraft = self.student_submission.IntercontinentalAircraft(40, "intercontinental", 100)
         self.assertEqual(aircraft.manifest,
                          f"Intercontinental flight intercontinental: passenger count 40, cargo load 100")
 
     def test_calculate_amount_of_fuel_short_haul(self):
-        aircraft = ShortHaulAircraft(90, "short")
+        aircraft = self.student_submission.ShortHaulAircraft(90, "short")
         fuel = aircraft.calculate_amount_of_fuel(1000)
         self.assertEqual(fuel, 9000)
 
     def test_get_manifest_short_haul(self):
-        aircraft = ShortHaulAircraft(40, "short")
+        aircraft = self.student_submission.ShortHaulAircraft(40, "short")
         serial_number = aircraft.get_serial_number()
         self.assertEqual(aircraft.manifest,
                          f"Short haul flight serial number {serial_number}, name short: passenger count 40")
 
     def test_list_flights(self):
-        intercontinental_flight = IntercontinentalAircraft(500, "Boeing-747", 100)
-        short_haul_flight = ShortHaulAircraft(110, "Airbus-A220")
-        short_haul_flight2 = ShortHaulAircraft(85, "Airbus-A220")
+        intercontinental_flight = self.student_submission.IntercontinentalAircraft(500, "Boeing-747", 100)
+        short_haul_flight = self.student_submission.ShortHaulAircraft(110, "Airbus-A220")
+        short_haul_flight2 = self.student_submission.ShortHaulAircraft(85, "Airbus-A220")
 
-        tower = ControlTower()
+        tower = self.student_submission.ControlTower()
         tower.add_aircraft(intercontinental_flight)
         tower.add_aircraft(short_haul_flight)
         tower.add_aircraft(short_haul_flight2)
